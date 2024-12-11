3be3c5279c530b380ad6146d34b9032c7b2e0406
piyush kumawat <piyushkumawat90607@gmail.com>
2024-12-11 12:42:52 +0530

    photo

--- resources/views/frontend/user/profile.blade.php
+++ resources/views/frontend/user/profile.blade.php
@@ -32,7 +32,7 @@
                     </div>
                 </div>
 
-                <!-- Photo -->
+                <!-- Photo --->
                 <div class="form-group row">
                     <label class="col-md-2 col-form-label fs-14">{{ translate('Photo') }}</label>
                     <div class="col-md-10">
